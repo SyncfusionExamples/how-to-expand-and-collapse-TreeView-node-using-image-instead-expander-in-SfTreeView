32cb554ac77ceb40711a5f39f1d8ce7c2be4503a
KamalaKannanSF4101 <kamal.sf4101@gmail.com>
2024-07-16 15:44:22 +0530

    Adding KB sample for SfTreeView

---
+++ MauiTreeView/Helper/Behavior.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.Maui.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiTreeView
+{
+    public class ExpanderIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Assembly assembly = typeof(App).GetTypeInfo().Assembly;
+            return (bool)value ? ImageSource.FromResource("MauiTreeView.Icons.expand.png", assembly) : ImageSource.FromResource("MauiTreeView.Icons.collapse.png", assembly);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class ExpanderIconVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Assembly assembly = typeof(App).GetTypeInfo().Assembly;
+            return (bool)value ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MauiTreeView/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.Maui.TreeView;
+using Syncfusion.TreeView.Engine;
+
+namespace MauiTreeView
+{
+    public partial class MainPage : ContentPage
+    {
+        SfTreeView TreeView;
+        public MainPage()
+        {
+            InitializeComponent();
+            TreeView = treeView;
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var imageIcon = sender as Image;
+            var treeViewNode = imageIcon!.BindingContext as TreeViewNode;
+            if (treeViewNode!.IsExpanded)
+                TreeView.CollapseNode(treeViewNode);
+            else
+                TreeView.ExpandNode(treeViewNode);
+        }
+    }
+}
